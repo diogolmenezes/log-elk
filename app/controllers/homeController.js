811764c3dbd827ed292d284c5845544b5b9bb4da
Diogo Menezes <diogo.leitao@oi.net.br>
2016-10-10 09:48:57 -0300

    trucando o log para buyan con json

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,5 +1,5 @@
 HomeController = function() {
-    this.log = require('../../config/log').getLogger("logElk");
+    this.log = require('../../config/log');
 };
 
 HomeController.prototype.index = function(request, response) {
@@ -9,7 +9,12 @@ HomeController.prototype.index = function(request, response) {
 HomeController.prototype.logar = function(request, response) {
    console.log('Gravando logs no ELK...');
 
-   var line = 'Testando o log no ELK' + new Date();
+   var line = {
+       nome: "Diogo Menezes",
+       idade: 31,
+       data: new Date()
+   }
+   
    this.log.info(line);
 
    response.end(line);
--- config/log.js
+++ config/log.js
@@ -1,30 +1,13 @@
-var log4js = require('log4js');
+var bunyan = require('bunyan');
 
-/*
- Sample logstash config:
-   udp {
-    codec => json
-    port => 10001
-    queue_size => 2
-    workers => 2
-    type => myAppType
-  }
-*/
-
-log4js.configure({
-  "appenders": [
+const logger = bunyan.createLogger({
+  name: 'log-elk',
+  streams: [
     {
-      type: "console",
-      category: "logElk"
-    },
-    {
-          "type": "file",
-          "filename": "./logs/log-elk.log",
-          "maxLogSize": 1024,
-          "backups": 3,
-          "category": "logElk"
+      level: 'info',
+      path: 'logs/log-elk.log'
     }
   ]
-});
+})
 
-module.exports = log4js;
+module.exports = logger;
